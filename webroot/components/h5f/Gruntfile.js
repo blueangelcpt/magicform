32d791ac80bb2e9583db518c0d713a9c01c28edb
Ian van den Heuvel <blueangelcpt@gmail.com>
2015-10-01 12:38:01 +0200

    Enhanced form validation for Safari. Updated H5F

--- webroot/components/h5f/Gruntfile.js
+++ webroot/components/h5f/Gruntfile.js
@@ -3,60 +3,60 @@ module.exports = function(grunt) {
   var browsers = grunt.file.readJSON("sauce-browsers.json");
   // Project configuration.
   grunt.initConfig({
-	qunit: {
-	  files: ['test/**/*.html']
-	},
-	connect: {
-	  server: {
-		options: {
-		  base: "",
-		  port: 9999
-		}
-	  }
-	},
-	'saucelabs-qunit': {
-		all: {
-			options: {
-				urls: ["http://127.0.0.1:9999/test/H5F.html"],
-				tunnelTimeout: 5,
-				build: process.env.TRAVIS_JOB_ID,
-				concurrency: 2,
-				browsers: browsers,
-				testname: "qunit tests",
-				tags: ["master"]
-			}
-		}
-	},
-	watch: {
-	  files: '<config:lint.files>',
-	  tasks: 'lint qunit'
-	},
-	jshint: {
-	  options: {
-		jshintrc: '.jshintrc'
-	  },
-	  all: [
-		'Gruntfile.js',
-		'src/{,*/}*.js',
-		'test/{,*/}*.js'
-	  ]
-	},
-	uglify: {
-	  options: {
-		stripbanners: true,
-		banner: '<%= banner%>',
-		mangle: {
-		  except: ['H5F']
-		},
-		preserveComments: 'some'
-	  },
-	  dist: {
-		src: [
-		  'src/H5F.js'
-		],
-		dest: 'h5f.min.js'
-	  }
-	}
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    connect: {
+      server: {
+        options: {
+          base: "",
+          port: 9999
+        }
+      }
+    },
+    'saucelabs-qunit': {
+        all: {
+            options: {
+                urls: ["http://127.0.0.1:9999/test/H5F.html"],
+                tunnelTimeout: 5,
+                build: process.env.TRAVIS_JOB_ID,
+                concurrency: 2,
+                browsers: browsers,
+                testname: "qunit tests",
+                tags: ["master"]
+            }
+        }
+    },
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'lint qunit'
+    },
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      all: [
+        'Gruntfile.js',
+        'src/{,*/}*.js',
+        'test/{,*/}*.js'
+      ]
+    },
+    uglify: {
+      options: {
+        stripbanners: true,
+        banner: '<%= banner%>',
+        mangle: {
+          except: ['H5F']
+        },
+        preserveComments: 'some'
+      },
+      dist: {
+        src: [
+          'src/H5F.js'
+        ],
+        dest: 'h5f.min.js'
+      }
+    }
   });
 
   // Load required contrib packages
